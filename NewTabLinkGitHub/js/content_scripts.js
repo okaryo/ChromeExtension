dbf4cf04f0c215c96211d8ecfaa9fd135cf44571
okaryo <okaryotr@gmail.com>
2020-05-21 22:37:17 +0900

    [fix] restrict target links

--- NewTabLinkGitHub/js/content_scripts.js
+++ NewTabLinkGitHub/js/content_scripts.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-  const githubLinks = document.getElementsByTagName('a')
+  const githubLinks = document.querySelector('.markdown-body a')
   for (let link of githubLinks) {
     link.target = '_blank'
     link.rel = 'noopener noreferrer'
   }
 
   chrome.runtime.onMessage.addListener((msg) => {
-    const links = document.getElementsByTagName('a')
+    const links = document.querySelector('.markdown-body a')
     for (let link of links) {
       link.target = msg.target
       link.rel = msg.rel
