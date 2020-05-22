101a9169408630f6018cca89e7313c717482ecf1
okaryo <okaryotr@gmail.com>
2020-05-22 11:13:17 +0900

    [fix] refactoring content scripts code

--- NewTabLinkGitHub/js/content_scripts.js
+++ NewTabLinkGitHub/js/content_scripts.js
@@ -1,12 +1,11 @@
 window.onload = () => {
-  const githubLinks = document.querySelector('.markdown-body a')
-  for (let link of githubLinks) {
+  const links = document.querySelectorAll('.markdown-body a')
+  for (let link of links) {
     link.target = '_blank'
     link.rel = 'noopener noreferrer'
   }
 
   chrome.runtime.onMessage.addListener((msg) => {
-    const links = document.querySelector('.markdown-body a')
     for (let link of links) {
       link.target = msg.target
       link.rel = msg.rel
